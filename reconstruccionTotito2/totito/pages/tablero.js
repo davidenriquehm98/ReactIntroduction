ecce5eddd351859bdceee2e572cbbb29f550a7aa
dehernandezm <davikik2chi5no@gmai.com>
2022-11-21 13:12:33 -0600

    cambio de diseño del tablero

--- reconstruccionTotito2/totito/pages/tablero.js
+++ reconstruccionTotito2/totito/pages/tablero.js
@@ -1,19 +1,22 @@
 const Tablero = () => {
   return (
-    <div className="grid grid-cols-3 gap-1 " >
-      {/* usaremos temporalmente unos div */}
-      <div className="h-5 w-5 bg-blue-400">1</div>
-      <div className="h-5 w-5 bg-blue-400">2</div>
-      <div className="h-5 w-5 bg-blue-400">3</div>
-      
-      <div className="h-5 w-5 bg-blue-400">4</div>
-      <div className="h-5 w-5 bg-blue-400">5</div>
-      <div className="h-5 w-5 bg-blue-400">6</div>
-      
-      <div className="h-5 w-5 bg-blue-400">7</div>
-      <div className="h-5 w-5 bg-blue-400">8</div>
-      <div className="h-5 w-5 bg-blue-400">9</div>
+    <div className="min-h-screen bg-slate-800 ">
+      <h1 className="text-8xl text-center mb-5 font-extrabold text-neutral-50 ">TOTITO</h1>
+      <div className="grid grid-rows-3 grid-flow-col gap-1 justify-center items-center">
 
+        <div className="w-52 h-52 rounded-md text-9xl bg-cyan-400 text-center cursor-pointer hover:bg-cyan-600">1</div>
+        <div className="w-52 h-52 rounded-md text-9xl bg-cyan-400 text-center cursor-pointer hover:bg-cyan-600">2</div>
+        <div className="w-52 h-52 rounded-md text-9xl bg-cyan-400 text-center cursor-pointer hover:bg-cyan-600">3</div>
+
+        <div className="w-52 h-52 rounded-md text-9xl bg-cyan-400 text-center cursor-pointer hover:bg-cyan-600">4</div>
+        <div className="w-52 h-52 rounded-md text-9xl bg-cyan-400 text-center cursor-pointer hover:bg-cyan-600">5</div>
+        <div className="w-52 h-52 rounded-md text-9xl bg-cyan-400 text-center cursor-pointer hover:bg-cyan-600">6</div>
+
+        <div className="w-52 h-52 rounded-md text-9xl bg-cyan-400 text-center cursor-pointer hover:bg-cyan-600">7</div>
+        <div className="w-52 h-52 rounded-md text-9xl bg-cyan-400 text-center cursor-pointer hover:bg-cyan-600">8</div>
+        <div className="w-52 h-52 rounded-md text-9xl bg-cyan-400 text-center cursor-pointer hover:bg-cyan-600">9</div>
+
+      </div>
     </div>
   )
 }
